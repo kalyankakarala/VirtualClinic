df20d6ea9c83a9ca6821f8d369abb335f4c2eec3
saikiran.ch <saikiran.c@digitelenetworks.com>
2021-03-31 12:20:49 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,43 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AddConsultationComponent } from './consultation/add-consultation/add-consultation.component';
 import { ConsultationComponent } from './consultation/consultation.component';
+import { EditConsultationComponent } from './consultation/edit-consultation/edit-consultation.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { AddPatientComponent } from './patient/add-patient/add-patient.component';
+import { EditPatientComponent } from './patient/edit-patient/edit-patient.component';
 import { PatientComponent } from './patient/patient.component';
 import { UserloginComponent } from './userlogin/userlogin.component';
 
 const routes: Routes = [
   {path: '', component: UserloginComponent},
   {path: 'login', component: UserloginComponent},
-  // {path: 'patient', component:PatientComponent},
-  {path: 'requestConsult', component: AddConsultationComponent},
   {
-    path: 'patient',
-   
+    path: 'patient', 
     children: [
       {
-        path: 'addpatient',
+        path: 'add',
         component: AddPatientComponent
+      },
+      {
+        path: 'edit',
+        component: EditPatientComponent
       },
       { path: '', component: PatientComponent, pathMatch: 'full'}
             ]
+},
+{
+  path: 'consultation', 
+  children: [
+    {
+      path: 'add',
+      component: AddConsultationComponent
+    },
+    {
+      path: 'edit',
+      component: EditConsultationComponent
+    },
+    { path: '', component: ConsultationComponent, pathMatch: 'full'}
+          ]
 },
 {path: '**', redirectTo: 'login', pathMatch: 'full'},
 ];
