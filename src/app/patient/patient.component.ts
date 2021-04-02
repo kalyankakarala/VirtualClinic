91a2f9ff69ef197de88f4de001ee4e6770d2c64d
saikiran.ch <saikiran.c@digitelenetworks.com>
2021-04-02 12:32:51 +0530

    removed navbar box as per discussion

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -24,8 +24,8 @@ export class PatientComponent implements OnInit {
    ELEMENT_DATA: PeriodicElement[] = [
     {patientId: 1, patientName: 'A', caseSummary : 'Diabetes', requestDate: '30/04/2021'},
     {patientId: 2, patientName: 'B', caseSummary : 'BP', requestDate: '05/04/2021'},
-    {patientId: 2, patientName: 'C', caseSummary : 'Nerve Weekness', requestDate: '06/04/2021'},
-    {patientId: 2, patientName: 'D', caseSummary : 'Fever', requestDate: '10/04/2021'},
+    {patientId: 3, patientName: 'C', caseSummary : 'Nerve Weekness', requestDate: '06/04/2021'},
+    {patientId: 4, patientName: 'D', caseSummary : 'Fever', requestDate: '10/04/2021'},
 
 
   ];
