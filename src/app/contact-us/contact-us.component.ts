6549b73209579a9415ae1ff762af79fff041946a
saikiran.ch <saikiran.c@digitelenetworks.com>
2021-04-13 21:09:24 +0530

    contact component changes

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  contactForm: FormGroup;
+ 
+  constructor(private formBuilder: FormBuilder) {
 
-  constructor() { }
-
-  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      Comment: new FormControl('',[Validators.required]),
+    });
+   }
+ 
+  ngOnInit(){
   }
 
+  get data() { return this.contactForm.controls}
+  
+contactFormSubmit(){
+console.log(this.contactForm.value)
+let data=  this.contactForm.value
+
+let obj = {
+  "comment": data.Comment
+}
+console.log(obj);
+
+}
 }
